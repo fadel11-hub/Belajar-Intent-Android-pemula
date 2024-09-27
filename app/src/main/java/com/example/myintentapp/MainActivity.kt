c66c7c5e055d34ab9647a7ca8ed95b1dec898f07
Fadel <fadelamili11@gmail.com>
2024-09-27 10:34:33 +0700

    Belajar parcilable

--- app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -13,6 +13,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnMoveWithObject: Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -21,6 +27,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val moveintent = Intent(this@MainActivity, MoveActivityDialog::class.java)
                 startActivity(moveintent)
             }
+
+            R.id.btn_move_activity_data -> {
+            val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+//                putExtra = mengirimkan data ke MoveWithDataActivity dengan objek intent yang merupakan metode key-value
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "dicodingAcademy Boy")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5)
+                startActivity(moveWithDataIntent)
+            }
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "DicodingAcademy",
+                    5,
+                    "john.mckinley@examplepetstore.com",
+                    "Bandung"
+                )
+            val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+            moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+            startActivity(moveWithObjectIntent)
+            }
+
+
         }
     }
 }
